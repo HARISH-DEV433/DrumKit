540b10ea4188078d447ee8a46cdb223d0da3e005
HARISH <112271304+HARISH-DEV433@users.noreply.github.com>
2024-01-21 21:44:37 +0530

    changes

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ for(var i = 0; i < buttonDrop; i++){
         
         var buttonHT = this.innerHTML;
         makeSound(buttonHT);
+        buttonAnimation(buttonHT);
     });
 }
 
 document.addEventListener("keypress", function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -46,4 +48,10 @@ function makeSound(key){
 }
 
 
-        
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    });
+}
